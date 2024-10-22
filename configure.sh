ca533aa9b57ac5d36a3141d99616028d2534e62a
Siddharth <siddharth@sidmb.com>
2024-10-21 22:24:50 -0700

    Add pico tinyusb sub-submodule

--- configure.sh
+++ configure.sh
@@ -1,5 +1,6 @@
 # Add submodule libraries (like pico-sdk)
 git submodule update --init
+(cd ./pico-sdk && git submodule update --init) # For TinyUSB submodule for USB support
 
 # For pico sdk
 mkdir -p $HOME/.pico-sdk/cmake/
