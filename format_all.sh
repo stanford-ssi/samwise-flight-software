b2f5ca2e08024b1e61d8899e2dbad5b27bbab0dd
Siddharth M. Bhatia <siddharth.m.bhatia@gmail.com>
2024-10-29 15:28:59 -0700

    Added: GitHub Workflow to automatically build the project, pre-commit hook to format all files on commit (#4)
    * Added build function
    
    * Updated configure.sh execution permissions
    
    * Changed capitalization in action
    
    * Add build directory during configure
    
    * Changed cmake command, added build product archiving
    
    * Add pico tinyusb sub-submodule
    
    * Github action - install arm gnu toolchain
    
    * configure.sh adds pre-commit hook for formatting
    
    * Instructions for installing clang-format mac
    
    * Changed pre-commit hook to hopefully add formatted files to git commit
    
    * Dummy commit to [hopefully] trigger pre-commit hook
    
    * Nil commit to [hopefully] trigger pre-commit hook
    
    * dummy commit to trigger pre-commit hook
    
    * Modify pre-commit hook to hopefully fix issue

--- format_all.sh
+++ format_all.sh
@@ -1,4 +1,5 @@
 # Helper script to recursively clang-format every file
+# Install clang-format on mac: brew install clang-format
 echo Formatting all files...
 find src -name "*.cpp" -o -name "*.c" -o -name "*.h" | xargs -I {} clang-format -i {}
 echo Done!
