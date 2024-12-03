5618277d072b3bfe8ba443ac7d7af3300d37cc08
Jackal-Studios <sashaluchyn@gmail.com>
2024-12-02 21:57:20 -0800

    Working but a lot of spaghetti for testing

--- src/main.c
+++ src/main.c
@@ -11,6 +11,8 @@
 #include "scheduler/scheduler.h"
 #include "slate.h"
 
+#include "state_machine/tasks/command_switch_task.h"
+
 /**
  * Statically allocate the slate.
  */
@@ -48,10 +50,105 @@ int main()
      * Go state machine!
      */
     LOG_INFO("main: Dispatching the state machine...");
-    while (true)
-    {
-        sched_dispatch(&slate);
+    // while (true)
+    // {
+    //     sched_dispatch(&slate);
+    // }
+    
+
+    /* TESTS */
+    struct test_t1DS{
+        int data_int_1;
+        uint8_t data_byteArr_1[16];
+    };
+    struct test_t1DS t1ds;
+    struct test_t1DS SEC_t1ds;
+    struct test_t1DS RECONt1ds;
+
+    struct test_t1DS FULLOUT;
+    struct test_t1DS FULLOUT2;
+
+    t1ds.data_int_1 = 229;
+    t1ds.data_byteArr_1[0] = 123;
+    t1ds.data_byteArr_1[14] = 249;
+    SEC_t1ds.data_int_1 = 303;
+    SEC_t1ds.data_byteArr_1[0] = 111;
+    SEC_t1ds.data_byteArr_1[9] = 222;
+
+    uint8_t data[251];
+    uint8_t data2[251];
+    uint8_t dataout[251];
+    uint8_t data2out[251];
+    uint8_t stopByte = 255;
+
+    data[0] = 1;
+    memcpy(&data[1], &t1ds, sizeof(t1ds));  // +1 for stop byte
+    // memcpy(&data[1] + sizeof(t1ds) + 1, &stopByte ,1);   // uncomment for separete queues
+    // check if reconstruction works?
+    memcpy(&RECONt1ds, &data[1], sizeof(RECONt1ds)); //% PAYLOAD_SIZE);
+    LOG_DEBUG("Reconstruction tested int: %i and byte at 14th: %i", RECONt1ds.data_int_1, RECONt1ds.data_byteArr_1[14]);
+
+
+
+
+    LOG_INFO("Successfully copied ds to byte arr");
+    command_switch_task_init(&slate);  // init queue
+    LOG_INFO("task init done");
+    sleep_ms(1000);
+    // queue_try_add(&slate.radio_packets_out, &data[0]); // add to radio queue
+    for(int i = 0; i < sizeof(t1ds); i++){
+                        LOG_DEBUG("input data1: %i", data[i]);
+                    }
+
+    data2[0] = 1;
+    memcpy(&data2[1], &SEC_t1ds, sizeof(SEC_t1ds)); 
+    memcpy(&data2[1]+ sizeof(SEC_t1ds) + 1, &stopByte, 1);
+
+    memcpy(&data[1] + sizeof(t1ds) + 1, &data2, sizeof(SEC_t1ds)+1);
+    queue_try_add(&slate.radio_packets_out, &data[0]); // add to radio queue
+
+for(int i = 0; i < sizeof(SEC_t1ds); i++){
+                        LOG_DEBUG("input data2: %i", data2[i]);
+                    }
+    // queue_try_add(&slate.radio_packets_out, &data2[0]); 
+    LOG_INFO("radio payload added");
+    sleep_ms(1000);
+    command_switch_dispatch(&slate);    // dequeue radio packet and add to task queue
+    LOG_INFO("dispatching done 1");
+    // command_switch_dispatch(&slate);    // dequeue radio packet and add to task queue
+    // LOG_INFO("dispatching done 2");
+    sleep_ms(1000);
+    queue_try_remove(&slate.task1_data,&dataout);
+    memcpy(&FULLOUT, &dataout, sizeof(FULLOUT)); //% PAYLOAD_SIZE);
+    LOG_DEBUG("Integer from 1st data: %i and byte at 14 pos: %i", FULLOUT.data_int_1, FULLOUT.data_byteArr_1[14]);
+
+    LOG_INFO("dequeued task data successfully");
+    sleep_ms(1000);
+
+
+    for (int i = 0; i < sizeof(t1ds); i++){
+        if (data[i+1] == dataout[i])
+        {
+           LOG_INFO("Correct coppying at %i", i);
+            // correctly copied up until i
+        }else{
+            LOG_INFO("Incorrect coppying at %i", i);
+            // incorectly copied at i 
+            //break;
+        }
+        LOG_INFO("data: %hhx", data[i+1]);
+        LOG_INFO("dataout: %hhx", dataout[i]);
+
+    
     }
+    queue_try_remove(&slate.task1_data,&data2out);
+    memcpy(&FULLOUT2, &data2out, sizeof(FULLOUT2)); //% PAYLOAD_SIZE);
+
+    LOG_INFO("dequeued task data2 successfully");
+    LOG_DEBUG("Integer (303, 111) from 2nd data: %i and byte at 0 pos: %i", FULLOUT2.data_int_1, FULLOUT2.data_byteArr_1[0]);
+
+
+    /* End of TESTS */
 
     /*
      * We should NEVER be here so something bad has happened.
