0d1f9e851cc1cc449425af4af64953f78ee22b3c
peppamint <chloezhong59@gmail.com>
2025-01-25 22:24:13 -0800

    watchdog_task.c and watchdog_task.h

---
+++ src/state_machine/tasks/watchdog_task.h
@@ -0,0 +1,16 @@
+/**
+ * @author  <Chloe Zhong>
+ * @date    <1/11/2025>
+ *
+ * <BRIEF DESCRIPTION OF TASK>
+ */
+
+#pragma once
+
+#include "slate.h"
+#include "state_machine/state_machine.h"
+
+void watchdog_task_init(slate_t *slate);
+void watchdog_task_dispatch(slate_t *slate);
+
+sm_task_t watchdog_task;
