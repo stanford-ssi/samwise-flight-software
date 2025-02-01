c71dd4a9d61118758e5ab7c282524c95b83a07b9
Michael Dalva <micdal06@gmail.com>
2025-02-01 13:23:48 -0800

    this configuration is able to send the packets, now we just need to parse them correctly

--- src/state_machine/tasks/radio_task.h
+++ src/state_machine/tasks/radio_task.h
@@ -17,7 +17,7 @@ typedef struct
     uint8_t dst;
     uint8_t flags;
     uint8_t seq;
-    uint8_t len;
+    uint8_t len; // this should be the length of the packet structure being sent over
     uint8_t data[252];
 } packet_t;
 
