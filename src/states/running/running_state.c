4b47aad78397db4ab6bb054703c0a9f54deb1a69
devYaoYH <yaoyiheng@gmail.com>
2025-02-24 21:32:57 -0800

    Run lint/formatter tool

--- src/states/running/running_state.c
+++ src/states/running/running_state.c
@@ -5,8 +5,9 @@ sched_state_t *running_get_next_state(slate_t *slate)
     return &running_state;
 }
 
-sched_state_t running_state = {
-    .name = "running",
-    .num_tasks = 6,
-    .task_list = {&print_task, &blink_task, &radio_task, &command_task, &beacon_task, &diagnostics_task},
-    .get_next_state = &running_get_next_state};
+sched_state_t running_state = {.name = "running",
+                               .num_tasks = 6,
+                               .task_list = {&print_task, &blink_task,
+                                             &radio_task, &command_task,
+                                             &beacon_task, &diagnostics_task},
+                               .get_next_state = &running_get_next_state};
