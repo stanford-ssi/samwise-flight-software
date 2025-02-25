7cda97bd3d5f3db2cc4d111a6969972d2c545969
Joseph Shetaye <joseph@shetaye.me>
2025-02-24 20:53:14 -0800

    Remove watchdog stuff

--- src/tasks/print/print_task.c
+++ src/tasks/print/print_task.c
@@ -6,9 +6,6 @@
 
 #include "print_task.h"
 
-#include "hardware/gpio.h"
-#include "pins.h"
-
 static uint32_t count = 0;
 
 void print_task_init(slate_t *slate)
@@ -20,9 +17,6 @@ void print_task_dispatch(slate_t *slate)
 {
     LOG_INFO("Test task is dispatching... %d", count);
     count++;
-    gpio_put(SAMWISE_WATCHDOG_FEED_PIN, 1);
-    sleep_ms(200);
-    gpio_put(SAMWISE_WATCHDOG_FEED_PIN, 0);
 }
 
 sched_task_t print_task = {.name = "print",
