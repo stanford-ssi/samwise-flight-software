7366b8bea426a21a13bb90b30122d81c3fe9e26a
malti <maltimjohn1@gmail.com>
2025-03-08 13:44:24 -0800

    /

--- src/tasks/sd-card-rw/sd-card_task.h
+++ src/tasks/sd-card-rw/sd-card_task.h
@@ -3,13 +3,10 @@
 #include "pico/stdlib.h"
 
 #include "slate.h"
-#include "state_machine.h"
-#include "typedefs.h"
-#include "sd_card.h"
-#include "ff.h"
+#include "sd-card.h"
 #include <stdio.h>
 
-void sd-card_task_init();
-void sd-card_task_test_rw();
+void sdcard_task_init();
+void sdcard_task_test_rw();
 
-extern sched_task_t sd-card_task;
+extern sched_task_t sdcard_task;
