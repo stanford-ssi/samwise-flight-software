644745d990b147f01e0226e4321b39136b1c1042
quack <quack@debian.marcar@stanford.edu>
2025-05-21 19:38:24 -0700

    Basis for command handling for Payload

---
+++ src/drivers/payload_command/payload_command.h
@@ -0,0 +1,16 @@
+/**
+ * @author Marc Aaron Reyes
+ * @date 2025-05-21
+ * 
+ */
+#include "slate.h"
+
+typedef int (*command_fn_t)();
+
+typedef struct {
+    const char *name;
+    command_fn_t fn;
+} command_t;
+
+bool init_payload(slate_t* slate);
+uint16_t payload_send_command(slate_t *slate, const uint8_t *command, uint8_t *report);
--- src/states/running/running_state.c
+++ src/states/running/running_state.c
@@ -6,9 +6,7 @@ sched_state_t *running_get_next_state(slate_t *slate)
 }
 
 sched_state_t running_state = {.name = "running",
-                               .num_tasks = 7,
-                               .task_list = {&print_task, &blink_task,
-                                             &radio_task, &command_task,
-                                             &beacon_task, &watchdog_task,
+                               .num_tasks = 4,
+                               .task_list = {&print_task, &blink_task, &watchdog_task,
                                             &payload_task},
                                .get_next_state = &running_get_next_state};
