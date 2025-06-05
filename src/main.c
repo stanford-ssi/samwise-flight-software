b4e39166e598256ad57b077194b4d048b068682d
devYaoYH <yaoyiheng@gmail.com>
2025-06-04 20:53:13 -0700

    Fixing minimum sleep_ms interval to work with how watchdog ON/OFF pattern requires

--- src/main.c
+++ src/main.c
@@ -15,6 +15,17 @@
 #include "scheduler.h"
 #include "slate.h"
 
+// Slate is initialized in slate.c
+extern slate_t slate;
+
+#ifndef PICO
+// Ensure that PICO_RP2350A is defined to 0 for PICUBED builds.
+// This is to enable full 48pin GPIO support on the RP2350A chip.
+// You'll have to overwrite this in your local pico-sdk directory.
+// samwise-flight-software/pico-sdk/src/boards/include/boards/pico2.h
+static_assert(PICO_RP2350A == 0,
+              "PICO_RP2350A must be defined to 0 for PICUBED builds.");
+#endif
 /**
  * Main code entry point.
  *
@@ -22,11 +33,22 @@
  */
 int main()
 {
-    /*
-     * Brief delay after reboot/powering up due to power spikes to prevent
-     * deployment when satellite is still within the launch mechanism.
-     */
-    safe_sleep_ms(5000);
+    // We need to first initialize watchdog before any sleep is called.
+    // Watchdog needs to be fed periodically to prevent rebooting.
+    slate.watchdog = watchdog_mk();
+    watchdog_init(&slate.watchdog);
+
+/*
+ * Brief delay after reboot/powering up due to power spikes to prevent
+ * deployment when satellite is still within the launch mechanism.
+ */
+#ifdef FLIGHT
+    // 15 minutes delay is required for dispersion after release from
+    // exolaunch deployment chute.
+    safe_sleep_ms(15 * 60 * 1000); // 15 minutes
+#else
+    safe_sleep_ms(5000); // 5 second for debugging
+#endif
 
     /*
      * Initialize persistent data or load existing data if already in flash.
@@ -36,15 +58,9 @@ int main()
     increment_reboot_counter();
     LOG_INFO("Current reboot count: %d\n", data->reboot_counter);
 
-/*
- * Initialize everything.
- */
-#ifndef PICO
-    // Ensure that PICO_RP2350A is defined to 0 for PICUBED builds.
-    // You'll have to overwrite this in your local pico-sdk directory.
-    // samwise-flight-software/pico-sdk/src/boards/include/boards/pico2.h
-    assert(PICO_RP2350A == 0);
-#endif
+    /*
+     * Initialize everything.
+     */
     LOG_DEBUG("main: Slate uses %d bytes", sizeof(slate));
     LOG_INFO("main: Initializing...");
     ASSERT(init(&slate));
--- src/utils/safe_sleep.h
+++ src/utils/safe_sleep.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include "common/config.h"
+#include "drivers/watchdog/watchdog.h"
+#include "pico/time.h"
 #include <slate.h>
 #include <stdint.h>
 
@@ -7,19 +10,15 @@ extern slate_t slate;
 
 static void safe_sleep_ms(uint32_t ms)
 {
-    const uint32_t safe_sleep_interval_ms = 10000; // 10 sec
     uint32_t remaining_time = ms;
 
-    watchdog_feed(&slate.watchdog);
-
-    while (remaining_time > 0)
+    while (remaining_time > MIN_WATCHDOG_INTERVAL_MS)
     {
         watchdog_feed(&slate.watchdog);
-        uint32_t time_to_sleep = remaining_time > safe_sleep_interval_ms
-                                     ? safe_sleep_interval_ms
-                                     : remaining_time;
-
-        remaining_time -= time_to_sleep;
-        sleep_ms(time_to_sleep);
+        // This together with the while loop condition ensures
+        // there will be no integer overflow.
+        remaining_time -= MIN_WATCHDOG_INTERVAL_MS;
+        sleep_ms(MIN_WATCHDOG_INTERVAL_MS);
     }
+    sleep_ms(remaining_time);
 }
