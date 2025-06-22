5a5334f5859863783d71e5bc6c2cc98a9985e480
bunny0514 <robotics123friends@gmail.com>
2025-06-22 12:15:09 -1000

    removed unnecessary define

--- src/drivers/solar/fixed_solar.h
+++ src/drivers/solar/fixed_solar.h
@@ -4,9 +4,6 @@
 #include "pins.h"
 #include "hardware/gpio.h"
 
-// BCM 13 (physical header 33?)
-#define GPIO13_SHIFT 9        
-
 void init_fixed_solar_charge();
 
 bool read_fixed_solar_charge();
