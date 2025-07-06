3c2f034abef4b7179e2d2418fd79b2afc76cd980
devYaoYH <yaoyiheng@gmail.com>
2025-07-06 10:45:08 -0700

    Added burn_wire task and state ready for remote overwrite into it

---
+++ src/states/burn_wire/burn_wire_state.c
@@ -0,0 +1,13 @@
+#include "burn_wire_state.h"
+
+extern sched_state_t burn_wire_state;
+
+sched_state_t *burn_wire_get_next_state(slate_t *slate)
+{
+    return &running_state;
+}
+
+sched_state_t burn_wire_state = {.name = "burn_wire",
+                                 .num_tasks = 1,
+                                 .task_list = {burn_wire_task},
+                                 .get_next_state = &burn_wire_get_next_state};
---
+++ src/tasks/burn_wire/burn_wire_task.c
@@ -0,0 +1,35 @@
+
+/**
+ * @author  Yao Yiheng
+ * @date    2025-07-06
+ */
+
+#include "burn_wire_task.h"
+
+static uint32_t count = 0;
+
+void burn_wire_task_init(slate_t *slate)
+{
+    LOG_INFO("Test task is initializing...");
+}
+
+void burn_wire_task_dispatch(slate_t *slate)
+{
+    LOG_INFO("Test task is dispatching... %d", count);
+    count++;
+    // Activate burn wire for 100ms at 50% duty cycle
+    // 0 -> off
+    // 1 -> 25% duty cycle
+    // 2 -> 50% duty cycle
+    // 3 -> 75% duty cycle
+    // 4 -> 100% duty cycle
+    burn_wire_activate(slate, MAX_BURN_DURATION_MS, 2, true, true);
+}
+
+sched_task_t burn_wire_task = {.name = "burn_wire",
+                               .dispatch_period_ms = 0, // Dispatch immediately
+                               .task_init = &burn_wire_task_init,
+                               .task_dispatch = &burn_wire_task_dispatch,
+
+                               /* Set to an actual value on init */
+                               .next_dispatch = 0};
