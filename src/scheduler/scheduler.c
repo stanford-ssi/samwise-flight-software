dd615fce3787680f36a5b2f64dfbbfa7d53098e2
devYaoYH <yaoyiheng@gmail.com>
2025-07-19 17:20:28 -0700

    Update scheduler variable references

--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -9,8 +9,6 @@
 #include "scheduler.h"
 #include "states.h"
 
-sched_state_t *overridden_state = NULL;
-
 /*
  * Include the actual state machine
  */
@@ -118,10 +116,10 @@ void sched_dispatch(slate_t *slate)
      * Transition to the next state, if required.
      */
     sched_state_t *next_state;
-    if (overridden_state)
+    if (slate->manual_override_state != NULL)
     {
-        next_state = overridden_state;
-        overridden_state = NULL;
+        next_state = slate->manual_override_state;
+        slate->manual_override_state = NULL;
     }
     else
     {
