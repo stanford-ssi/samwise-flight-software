ae9184f5c9badbddd3cb60cf5d3965dcd4ddb1f7
Sanjay Somasundaram <106445031+microbites0@users.noreply.github.com>
2025-11-15 13:50:29 -0800

    Update src/init/init.c
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/init/init.c
+++ src/init/init.c
@@ -97,7 +97,6 @@ bool init(slate_t *slate)
     gpio_set_dir(SAMWISE_FIXED_SOLAR_CHARGE, GPIO_IN);
     gpio_set_dir(SAMWISE_FIXED_SOLAR_FAULT, GPIO_IN);
     gpio_set_dir(SAMWISE_PANEL_A, GPIO_IN);
-    gpio_init(SAMWISE_PANEL_B);
     gpio_set_dir(SAMWISE_PANEL_B, GPIO_IN);
     gpio_init(SAMWISE_RBF_DETECT_PIN);
     gpio_set_dir(SAMWISE_RBF_DETECT_PIN, GPIO_IN);
