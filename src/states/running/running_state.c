8df68b0c5336cd8039aca987faf9bebaf19071f5
devYaoYH <yaoyiheng@gmail.com>
2026-02-21 14:22:14 -0800

    Removed some unused mock files

--- src/states/running/running_state.c
+++ src/states/running/running_state.c
@@ -19,7 +19,9 @@ sched_state_t running_state = {
     .name = "running",
     .id = STATE_RUNNING,
     .num_tasks = 2,
-    .task_list = {&print_task, &blink_task}, // enable radio when we have the test board working
+    .task_list =
+        {&print_task,
+         &blink_task}, // enable radio when we have the test board working
     .get_next_state = &running_get_next_state};
 #else
 sched_state_t running_state = {
--- src/test_mocks/running_state.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "state_machine.h"
-
-// Mock running_state for tests
-extern sched_state_t running_state;
