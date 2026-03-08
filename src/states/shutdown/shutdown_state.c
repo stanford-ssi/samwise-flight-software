ddeaaae687ed1f84657d6389f37524f56284254e
Luis C <lc0001coll@gmail.com>
2026-03-07 16:43:28 -0800

    added 3 month timer to escape shutdown.

--- src/states/shutdown/shutdown_state.c
+++ src/states/shutdown/shutdown_state.c
@@ -4,10 +4,21 @@
 
 #define SHUTDOWN_STATE_COLOR 75, 0, 130 // Indigo: shutdown indicator
 
+// 3 months in milliseconds: 90 days * 24h * 60m * 60s * 1000ms
+#define SHUTDOWN_TIMEOUT_MS (90ULL * 24 * 60 * 60 * 1000)
+
 state_id_t shutdown_get_next_state(slate_t *slate)
 {
-    (void)slate;
     neopixel_set_color_rgb(SHUTDOWN_STATE_COLOR);
+
+    if (slate->time_in_current_state_ms >= SHUTDOWN_TIMEOUT_MS)
+    {
+        LOG_INFO("Shutdown timeout reached (3 months). Reinitializing.");
+        slate->shutdown_triggered = false;
+        slate->shutdown_cmd_counter = 0;
+        return STATE_INIT;
+    }
+
     return STATE_SHUTDOWN;
 }
 
