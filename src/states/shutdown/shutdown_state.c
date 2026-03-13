5eeb9c21eb935a273f1d86bdf3a0509c212a024b
Luis C <lc0001coll@gmail.com>
2026-03-12 18:14:34 -0700

    Update src/states/shutdown/shutdown_state.c. to match format suggestion
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/states/shutdown/shutdown_state.c
+++ src/states/shutdown/shutdown_state.c
@@ -22,9 +22,8 @@ state_id_t shutdown_get_next_state(slate_t *slate)
     return STATE_SHUTDOWN;
 }
 
-sched_state_t shutdown_state = {
-    .name = "shutdown",
-    .id = STATE_SHUTDOWN,
-    .num_tasks = 1,
-    .task_list = {&watchdog_task},
-    .get_next_state = &shutdown_get_next_state};
+sched_state_t shutdown_state = {.name = "shutdown",
+                                .id = STATE_SHUTDOWN,
+                                .num_tasks = 1,
+                                .task_list = {&watchdog_task},
+                                .get_next_state = &shutdown_get_next_state};
