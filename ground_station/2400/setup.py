1a581c902da61e7c4a9eab7a01868b4a6d552b93
Luis C <lc0001coll@gmail.com>
2026-04-24 18:37:01 -0700

    Made changes to fix gpio_read errror

--- ground_station/2400/setup.py
+++ ground_station/2400/setup.py
@@ -7,4 +7,8 @@ def initialize():
     gpio.setwarnings(False)
 
     # Radio enable
-    gpio.setup(RADIO_2400_ENABLE, gpio.OUT)
+    # NOTE: initial= must be passed explicitly. On Pi OS Bookworm/Trixie the
+    # RPi.GPIO package is the rpi-lgpio shim, which (without initial=) tries
+    # to gpio_read() the pin before claiming it and raises
+    # `lgpio.error: 'GPIO not allocated'`.
+    gpio.setup(RADIO_2400_ENABLE, gpio.OUT, initial=gpio.LOW)
